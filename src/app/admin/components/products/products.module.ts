b1a908ad88a07e345dec7c993a51c707cee1427d
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2022-12-31 15:28:54 +0300

    add a product or list them

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CreateComponent } from './create/create.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    CreateComponent
   ],
   imports: [
     CommonModule,
+    MatSidenavModule,
+    MatFormFieldModule,
     RouterModule.forChild([
       {path:"",component:ProductsComponent}
     ])
