bf20ad8d46c1e6c18cc3f54850684c93415c04a1
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2022-12-31 21:59:29 +0300

    remainder of adding product work

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -3,20 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { CreateComponent } from './create/create.component';
-
+import { ListComponent } from './list/list.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    CreateComponent
+    CreateComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
     MatSidenavModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forChild([
       {path:"",component:ProductsComponent}
     ])
